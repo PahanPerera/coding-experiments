eb6fce2ea60007d6cc504f3420883a8198c8123d
Pahan Madushanka Perera <pahan.madushanka@gmail.com>
2021-10-15 00:52:05 -0400

    use of hooks for tower of hanoi

--- src/tower-of-hanoi/Main.js
+++ src/tower-of-hanoi/Main.js
@@ -1,41 +1,11 @@
-import { useEffect, useState } from "react";
 import styles from "./Main.module.css";
 import Tower from "./components/Tower";
-import { prepareHanoi, solveHanoi } from "./Solver";
+import useGameSteps from "./hooks/useGameSteps";
 
 const TOWER_HEIGHT = 5;
 
 function TowerOfHanoi() {
-  let [step, setStep] = useState([[], [], []]);
-  let [moves, setMoves] = useState(0);
-  let [appState, setAppState] = useState("NOT_STARTED");
-  let [loop, setLoop] = useState(null);
-
-  useEffect(() => {
-    setStep(prepareHanoi(TOWER_HEIGHT));
-    setMoves(0);
-    return () => {
-      if (loop) clearInterval(loop);
-    };
-  }, [loop]);
-
-  let start = function () {
-    if (appState === "STARTED") return;
-    setAppState("STARTED");
-    let steps = solveHanoi(TOWER_HEIGHT);
-    let index = 0;
-    let loopHandler = setInterval(() => {
-      if (index === steps.length) {
-        clearInterval(loopHandler);
-        setAppState("FINISHED");
-        return;
-      }
-      setStep(steps[index]);
-      index++;
-      setMoves(++moves);
-    }, 700);
-    setLoop(loopHandler);
-  };
+  const { step, moves, startFunction: start } = useGameSteps(TOWER_HEIGHT);
 
   return (
     <main className={styles.container}>
---
+++ src/tower-of-hanoi/hooks/useGameSteps.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { prepareHanoi, solveHanoi } from "../Solver";
+
+function useGameSteps(height) {
+  let [step, setStep] = useState([[], [], []]);
+  let [moves, setMoves] = useState(0);
+  let [isStarted, setIsStarted] = useState(false);
+  let [loop, setLoop] = useState(null);
+
+  useEffect(() => {
+    setStep(prepareHanoi(height));
+    setMoves(0);
+    return () => {
+      if (loop) clearInterval(loop);
+    };
+  }, [loop]);
+
+  let start = function () {
+    if (isStarted) return;
+    setIsStarted(true);
+    let steps = solveHanoi(height);
+    let index = 0;
+    let loopHandler = setInterval(() => {
+      if (index === steps.length) {
+        clearInterval(loopHandler);
+        setIsStarted(false);
+        return;
+      }
+      setStep(steps[index]);
+      index++;
+      setMoves(++moves);
+    }, 700);
+    setLoop(loopHandler);
+  };
+
+  return {
+    step,
+    moves,
+    startFunction: start,
+  };
+}
+
+export default useGameSteps;
