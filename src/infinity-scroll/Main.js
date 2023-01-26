cbc3fd2185ceb737b814edf9cb14288877f95640
Pahan Madushanka Perera <pahan.madushanka@gmail.com>
2023-01-25 22:15:02 -0500

    optimizing infinity scroll

--- src/infinity-scroll/Main.js
+++ src/infinity-scroll/Main.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useRef } from "react";
 import { useState } from "react";
 import styles from "./Main.module.css";
@@ -9,22 +9,28 @@ const postTemplate = (index) => ({
   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
 });
 
+const loadPostsFromApi = (size, cursor = 0) => {
+  const newPosts = [];
+  for (let i = 0; i < size; i++) {
+    newPosts.push(postTemplate(cursor + i + 1));
+  }
+  return newPosts;
+};
+
 const usePosts = () => {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const loadNext = (size) => {
+  const loadNext = useCallback((size) => {
     setIsLoading(true);
-    const newPosts = [];
-    const len = posts.length;
-    for (let i = 0; i < size; i++) {
-      newPosts.push(postTemplate(len + i));
-    }
     setTimeout(() => {
       setIsLoading(false);
-      setPosts([...posts, ...newPosts]);
-    }, 2000);
-  };
+      setPosts((posts) => {
+        const newPosts = loadPostsFromApi(size, posts.length);
+        return [...posts, ...newPosts];
+      });
+    }, 5000);
+  }, []);
 
   return { data: posts, loadNext, isLoading };
 };
@@ -33,24 +39,33 @@ const useInfinityScroll = () => {
   const containerRef = useRef();
   const [initialSize, setInitialSize] = useState(0);
   const [hasReachedEnd, setHasReachedEnd] = useState(false);
+  const containerScrollHeightRef = useRef(0);
+
+  const scrollEventHandler = useCallback(() => {
+    if (
+      Math.floor(
+        containerRef.current.scrollTop + containerRef.current.offsetHeight
+      ) >= containerRef.current.scrollHeight
+    ) {
+      setHasReachedEnd(true);
+    } else {
+      setHasReachedEnd(false);
+    }
+  }, []);
 
   useEffect(() => {
     if (containerRef.current) {
+      const element = containerRef.current;
       const h = containerRef.current.offsetHeight;
+      containerScrollHeightRef.current = containerRef.current.scrollHeight;
       const s = Math.floor(h / 100) + 2;
       setInitialSize(s);
-      containerRef.current.addEventListener("scroll", (e) => {
-        if (
-          containerRef.current.scrollTop + containerRef.current.offsetHeight >=
-          containerRef.current.scrollHeight
-        ) {
-          setHasReachedEnd(true);
-        } else {
-          setHasReachedEnd(false);
-        }
-      });
+      element.addEventListener("scroll", scrollEventHandler);
+      return () => {
+        element.removeEventListener("scroll", scrollEventHandler);
+      };
     }
-  }, [containerRef.current]);
+  }, [scrollEventHandler]);
 
   return { containerRef, initialSize, hasReachedEnd };
 };
@@ -63,13 +78,13 @@ function InfinityScroll() {
     if (initialSize > 0) {
       loadNext(initialSize);
     }
-  }, [initialSize]);
+  }, [initialSize, loadNext]);
 
   useEffect(() => {
-    if (hasReachedEnd) {
+    if (hasReachedEnd & !isLoading) {
       loadNext(5);
     }
-  }, [hasReachedEnd]);
+  }, [hasReachedEnd, loadNext, isLoading]);
 
   return (
     <main className={styles.container} ref={containerRef}>
